c23253cdb83fe0f28d664f7192c76b7ec3e1e007
queerviolet <Ashi Krishnan me@ashi.works>
2017-05-05 12:39:10 -0400

    still needs work and a runner, but it parses now.

--- index.js
+++ index.js
@@ -12,33 +12,40 @@ const {
       , $
       } = require('./parse')
 
-const Identifier = $(OneOrMore(CharIn('a', 'z')))
-    , __ = Drop(ZeroOrMore(Or(Exactly(' '))))
-    , InfixOperatorPrecedenceGroup = (type, [...operators], NextPrecedenceGroup=Identifier) => Sequence(__
-        , As(NextPrecedenceGroup, 'first'), __
-        , As(
-            OneOrMore(Sequence(
-                As(Or(...operators.map(Exactly)), 'operator'), __
-              , As(NextPrecedenceGroup, 'rhs'), __
-            )),
-            'rest')
+const __ = Drop(ZeroOrMore(Or(Exactly(' '))))
+    , Optional = read => Many(read, {min: 0, max: 1})
+    , Identifier = Sequence(__
+        , As($(OneOrMore(CharIn('a', 'z'))), 'identifier')
         , state => Object.assign({}, state, {
-          match: Object.assign({type}, state.match)
-        })        
-      )
-    // , AdditiveTerm = Sequence(__
-    //     , As(Identifier, 'first'), __
-    //     , As(
-    //         OneOrMore(Sequence(
-    //             As(Or(Exactly('+'), Exactly('-')), 'operator'), __
-    //           , As(Identifier, 'rhs'), __
-    //         )),
-    //         'rest')
-    //     , state => Object.assign({}, state, {
-    //       match: Object.assign({type: 'Add'}, state.match)
-    //     })        
-    //   )
-    , AdditiveTerm = InfixOperatorPrecedenceGroup('additive', ['+', '-'])
+          match: Object.assign({type: 'Identifier'}, state.match)
+        }))
+    , Digit = CharIn('0', '9')
+    , Sign = Optional(Exactly('-'))
+    , Literal = Sequence(__
+        , As(Sign, 'sign')
+        , As($(Many(Digit)), 'digits')
+        , state => Object.assign({}, state, {
+          match: Object.assign({type: 'Literal'}, state.match)
+        })
+    )
+    , IdentifierOrLiteral = Or(Identifier, Literal)
+    , InfixOperatorPrecedenceGroup = (
+        type, [...operators], NextPrecedenceGroup=IdentifierOrLiteral) =>
+        Or(Sequence(__
+              , As(NextPrecedenceGroup, 'first'), __
+              , As(
+                  OneOrMore(Sequence(
+                      As(Or(...operators.map(Exactly)), 'operator'), __
+                    , As(NextPrecedenceGroup, 'rhs'), __
+                  )),
+                  'rest')
+              , state => Object.assign({}, state, {
+                match: Object.assign({type}, state.match)
+              })        
+            )
+            , NextPrecedenceGroup)
+    , MultiplicativeTerm = InfixOperatorPrecedenceGroup('mul', ['*', '/'])
+    , AdditiveTerm = InfixOperatorPrecedenceGroup('add', ['+', '-'], MultiplicativeTerm)
     , Expression = Or(AdditiveTerm, Identifier)
     , Assignment = Sequence(__
         , As(Identifier, 'lhs'), __
@@ -51,6 +58,6 @@ const Identifier = $(OneOrMore(CharIn('a', 'z')))
 
     , Start = ZeroOrMore(Sequence(Or(Assignment, Expression), Drop(ZeroOrMore(Exactly('\n')))))
 
-console.log(JSON.stringify(Start({input: 'x = y\nx + z + k\nf+n\n'}), 0, 2))
+console.log(JSON.stringify(Start({input: 'x = -2 * y\nx * z + k\nf+n\n'}), 0, 2))
 
 module.exports = {Many, CharIn, Start, Assignment, Expression}
